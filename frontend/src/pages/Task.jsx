6d8eae1ae99943728b9246916dfd4b346ef96f72
Mishin Alexey <mishin-lex@mail.ru>
2020-11-14 05:12:08 +0300

    Add react-draggable and connecting arrow

--- frontend/src/pages/Task.jsx
+++ frontend/src/pages/Task.jsx
@@ -1,15 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from 'react';
+import Draggable from 'react-draggable';
 import { cnb } from 'cnbuilder';
 import Slide from '@material-ui/core/Slide';
-import { Typography, Grid } from "@material-ui/core";
+import { Typography, Grid } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import { ArrowBack, Close } from '@material-ui/icons';
 
-import Select from "../components/Select";
+import Select from '../components/Select';
 import { MODELS as EID_MODELS } from '../entities/device/IEDDevice';
-import List from "../components/List";
-import Card from "../components/Card";
-import { useCallback } from "react";
+import List from '../components/List';
+import Card from '../components/Card';
+import TaskPlayground from '../components/TaskPlayground';
 
 const useStyles = makeStyles((theme) => ({
     header: {
@@ -105,30 +106,62 @@ const Task = () => {
     return (
         <div className={classes.wrapper}>
             <Card>
-                <Typography className={classes.header} variant="h6">Выбор устройства</Typography>
-                <Select options={categoryOptions} value={category} onChange={onChangeCategory} />
+                <Typography className={classes.header} variant="h6">
+                    Выбор устройства
+                </Typography>
+                <Select
+                    options={categoryOptions}
+                    value={category}
+                    onChange={onChangeCategory}
+                />
             </Card>
-            <Slide direction="down" in={isCategoryChosen} mountOnEnter unmountOnExit>
+            <Slide
+                direction="down"
+                in={isCategoryChosen}
+                mountOnEnter
+                unmountOnExit
+            >
                 <Card>
-                    <button onClick={onClearCategory} className={cnb(classes.clearButton, classes.button)}>
+                    <button
+                        onClick={onClearCategory}
+                        className={cnb(classes.clearButton, classes.button)}
+                    >
                         <ArrowBack />
                     </button>
-                    <List items={category?.devices} onChange={onChangeDetailedCategory} />
+                    <List
+                        items={category?.devices}
+                        onChange={onChangeDetailedCategory}
+                    />
                 </Card>
             </Slide>
-            <Slide direction="down" in={isDetailedDeviceChosen} mountOnEnter unmountOnExit>
+            <Slide
+                direction="down"
+                in={isDetailedDeviceChosen}
+                mountOnEnter
+                unmountOnExit
+            >
                 <Card>
                     <Grid container direction="column" alignItems="center">
                         <Grid container item alignItems="flex-start">
                             <Typography variant="h6">
                                 {detailedDevice?.label}
                             </Typography>
-                            <button onClick={onClearDetailedDevice} className={cnb(classes.closeButton, classes.button)}>
+                            <button
+                                onClick={onClearDetailedDevice}
+                                className={cnb(
+                                    classes.closeButton,
+                                    classes.button,
+                                )}
+                            >
                                 <Close className={classes.closeButtonIcon} />
                             </button>
                         </Grid>
                         <Grid item>
-                            <img src={detailedDevice?.img} alt={detailedDevice?.label} className={classes.deviceImage} />
+                            <img
+                                src={detailedDevice?.img}
+                                alt={detailedDevice?.label}
+                                className={classes.deviceImage}
+                            />
                         </Grid>
                         <Grid item>
                             <p className={classes.deviceDetails}>
@@ -138,9 +171,9 @@ const Task = () => {
                     </Grid>
                 </Card>
             </Slide>
+            <TaskPlayground />
         </div>
-    )
+    );
 };
-
 
 export default Task;
