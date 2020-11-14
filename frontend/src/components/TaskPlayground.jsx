fac522885a7c0aa90ed0d94244a4bb054ae0d92f
xiagn17 <xiagn17@gmail.com>
2020-11-15 02:50:43 +0300

    Fix playground

--- frontend/src/components/TaskPlayground.jsx
+++ frontend/src/components/TaskPlayground.jsx
@@ -66,7 +66,7 @@ export default function TaskPlayground({ devices }) {
                 );
             })}
             <GooseSettings devices={devices}/>
-        </div>
+        </Container>
     );
 }
 
