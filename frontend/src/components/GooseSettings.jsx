dcd00599c5f9b3d46f116ef6713759781bd4d86a
Mishin Alexey <mishin-lex@mail.ru>
2020-11-15 06:15:07 +0300

    Remove absolute window

--- frontend/src/components/GooseSettings.jsx
+++ frontend/src/components/GooseSettings.jsx
@@ -1,43 +1,36 @@
-import React  from 'react';
+import React from 'react';
 
 import styled from 'styled-components';
-import TableButton from "./common/TableButton";
-import GooseSettingsDialog from "./dialogs/GooseSettingsDialog";
-import IED_PARAMETERS from "../const/IED_PARAMETERS";
+import TableButton from './common/TableButton';
+import GooseSettingsDialog from './dialogs/GooseSettingsDialog';
+import IED_PARAMETERS from '../const/IED_PARAMETERS';
 import { useDialog } from '../hooks/useDialog';
 
-const GooseAbsoluteWindow = styled.div`
-position: fixed;
-    top: 70px;
-    left: 0;
-    width: 100vw;
-    height: calc(100vh - 170px);
-`;
-
 const GooseButton = styled(TableButton)`
-padding: 0 15px;
-position: absolute;
-bottom: 40px;
-left: 50%;
-transform: translateX(-50%);
-:hover{
-cursor: pointer;
-}
+    padding: 0 15px;
+    position: fixed;
+    bottom: 124px;
+    left: 50%;
+    transform: translateX(-50%);
+    z-index: 1000;
+    :hover {
+        cursor: pointer;
+    }
 `;
 
 const GooseApply = styled(TableButton)`
-padding: 0 15px;
-position: absolute;
+    padding: 0 15px;
+    position: absolute;
 
-top: 30px;
-right: 110px;
+    top: 30px;
+    right: 110px;
 
-:hover{
-cursor: pointer;
-}
+    :hover {
+        cursor: pointer;
+    }
 
-background: #2A5EA1;
-color: #FFFFFF;
+    background: #2a5ea1;
+    color: #ffffff;
 `;
 const GooseSettings = (props) => {
     const { devices } = props;
@@ -46,14 +39,10 @@ const GooseSettings = (props) => {
 
     const onGooseApply = () => {
         const devicesSettings = devices
-            .filter(d => d.type === 'ied')
-            .map(({networkData, gcb, goooseId, macAddress, appId, vlanId, minTime, maxTime, name}) => {
-                const l = name.length;
-                const first = name.slice(l - 1, l) === '1';
-                const second = name.slice(l - 1, l) === '2';
-
-                return {
-                    ...networkData,
+            .filter((d) => d.type === 'ied')
+            .map(
+                ({
+                    networkData,
                     gcb,
                     goooseId,
                     macAddress,
@@ -61,31 +50,57 @@ const GooseSettings = (props) => {
                     vlanId,
                     minTime,
                     maxTime,
-                    type: first ? 'first' : (second ? 'second' : ''),
-                };
-            }).filter(d => d.type !== '');
+                    name,
+                }) => {
+                    const l = name.length;
+                    const first = name.slice(l - 1, l) === '1';
+                    const second = name.slice(l - 1, l) === '2';
+
+                    return {
+                        ...networkData,
+                        gcb,
+                        goooseId,
+                        macAddress,
+                        appId,
+                        vlanId,
+                        minTime,
+                        maxTime,
+                        type: first ? 'first' : second ? 'second' : '',
+                    };
+                },
+            )
+            .filter((d) => d.type !== '');
         if (devicesSettings.length !== 2) {
-            console.log('nedostatochno')
+            console.log('nedostatochno');
             return; // ошибкаа Недостаточно данных о двух коммутаторах
         }
         const isAllAnswersRight = devicesSettings.reduce((acc, settings) => {
             const rightAnswers = IED_PARAMETERS[settings.type];
-            const isAllRight = Object.keys(rightAnswers)
-                .map(key => {
-                    return rightAnswers[key] === settings[key];
-                })
-                .filter((cur) => !cur).length === 0;
+            const isAllRight =
+                Object.keys(rightAnswers)
+                    .map((key) => {
+                        return rightAnswers[key] === settings[key];
+                    })
+                    .filter((cur) => !cur).length === 0;
             return acc && isAllRight;
         }, true);
 
         console.log('isAllAnswersRight', isAllAnswersRight);
     };
     return (
-        <GooseAbsoluteWindow>
-            <GooseSettingsDialog open={dialog.open} devices={devices} onClose={dialog.onClose} />
-            <GooseButton onClick={dialog.onOpen} >Подписки GOOOSE-сообщений</GooseButton>
-            <GooseApply onClick={onGooseApply}>Отправить GOOOSE-сообщений</GooseApply>
-        </GooseAbsoluteWindow>
+        <>
+            <GooseSettingsDialog
+                open={dialog.open}
+                devices={devices}
+                onClose={dialog.onClose}
+            />
+            <GooseButton onClick={dialog.onOpen}>
+                Подписки GOOOSE-сообщений
+            </GooseButton>
+            <GooseApply onClick={onGooseApply}>
+                Отправить GOOOSE-сообщений
+            </GooseApply>
+        </>
     );
 };
 
